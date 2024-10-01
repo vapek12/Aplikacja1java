7a0625f7452cf2f2173f98c119c0acc67ed9861f
DESKTOP-DSKJUIM\vapek12 <Adamkaleta2006@gmail.com>
2024-10-01 11:02:16 +0200

    zmienna handleSubmit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 function App() {
   const [tytul, setTytul] = useState("");
   const [rodzaj, setRodzaj] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Tytul: ' + tytul + ', Rodzaj: ' + rodzaj);
+  }
 }
 
 export default App;
