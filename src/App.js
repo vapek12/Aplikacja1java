ee244b31b22ec3693a648cd88d7cecac724e9e4a
DESKTOP-DSKJUIM\vapek12 <Adamkaleta2006@gmail.com>
2024-10-01 11:04:54 +0200

    komentarze

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+  // Stany przechwytujące dane formularza
   const [tytul, setTytul] = useState("");
   const [rodzaj, setRodzaj] = useState("");
 
+  // Fumkcja obsługująca przycisk "Dodaj"
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Zapobiega odśweirzaniu strony po kliknięciu "Dodaj"
+
+    // Wyświetlanie ddanych formularza w konsoli
     console.log('Tytul: ' + tytul + ', Rodzaj: ' + rodzaj);
-  }
+  };
 }
 
 export default App;
